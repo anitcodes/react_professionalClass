8842e342835cb45850acd02bf2df27d94c60c987
Anit Shrestha <anitshrestha101@gmail.com>
2025-08-06 07:03:00 +0545

    reset button added to react_basic

--- react_basic/src/App.jsx
+++ react_basic/src/App.jsx
@@ -9,43 +9,56 @@ function App() {
   const [hobby, setHobby] = useState();
 
   return (
-    <div className="box">
-      <h1>Enter your details:</h1>
+    <>
+      <div className="box">
+        <h1>Enter your details:</h1>
 
-      <div className="box-contents">
-        <h2>Full Name:</h2>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
+        <div className="box-contents">
+          <h2>Full Name:</h2>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
 
-        <h2>Favorite Color:</h2>
-        <input
-          type="text"
-          value={color}
-          onChange={(e) => {
-            setColor(e.target.value);
-          }}
-        />
+          <h2>Favorite Color:</h2>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
 
-        <h2>Hobby:</h2>
-        <input
-          type="text"
-          value={hobby}
-          onChange={(e) => {
-            setHobby(e.target.value);
-          }}
-        />
+          <h2>Hobby:</h2>
+          <input
+            type="text"
+            value={hobby}
+            onChange={(e) => {
+              setHobby(e.target.value);
+            }}
+          />
+        </div>
+
+        {/* Passing props to child components */}
+        <Name name={name} />
+        <Color color={color} />
+        <Hobby hobby={hobby} />
       </div>
 
-      {/* Passing props to child components */}
-      <Name name={name} />
-      <Color color={color} />
-      <Hobby hobby={hobby} />
-    </div>
+      <button
+        onClick={() => {
+          setName("");
+          setColor("");
+          setHobby("");
+        }}
+        className="reset"
+      >
+        Reset
+      </button>
+    </>
   );
 }
 
--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -13,21 +13,41 @@
 
 // export default App
 
-
 // UseState in React
-import React from 'react'
-// import Welcome from './components/Welcome'
-// import Counter from './components/Counter'
-// import Post from './components/Post'
+// import React from 'react'
+// // import Welcome from './components/Welcome'
+// // import Counter from './components/Counter'
+// // import Post from './components/Post'
 
-const App = () => {
-  return (
-    <>
-    {/* <Welcome faculty = "CSIT" /> */}
-    {/* <Counter /> */}
-    {/* <Post postTitle = "Learn React Basic" /> */}
-    </>
-  )
-}
+// const App = () => {
+//   return (
+//     <>
+//     {/* <Welcome faculty = "CSIT" /> */}
+//     {/* <Counter /> */}
+//     {/* <Post postTitle = "Learn React Basic" /> */}
+//     </>
+//   )
+// }
 
-export default App
+// export default App
+
+
+// useEffect in React
+// import React, { useEffect, useState } from "react";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     console.log("Component rendered or re-rendered");
+//   },[count]);
+
+//   return (
+//     <>
+//       <p>You Clicked {count} times</p>
+//       <button onClick={() => setCount(count+1)} >Click Me</button>
+//     </>
+//   );
+// };
+
+// export default App;
