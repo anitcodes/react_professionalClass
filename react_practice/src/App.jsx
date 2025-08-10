2a7b733d226ca2abc59646ce6b2df1581b466e6c
Anit Shrestha <anitshrestha101@gmail.com>
2025-08-10 19:36:09 +0545

    react_practice folder initialized

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,68 +1,8 @@
-// Props in REACT
-// import React from 'react'
-// import Welcome from './components/Welcome'
-
-// const App = () => {
-//   return (
-// <>
-// <Welcome faculty="CSIT" />
-// <Welcome />
-// </>
-//   )
-// }
-
-// export default App
-
-// UseState in React
-// import React from 'react'
-// // import Welcome from './components/Welcome'
-// // import Counter from './components/Counter'
-// // import Post from './components/Post'
-
-// const App = () => {
-//   return (
-//     <>
-//     {/* <Welcome faculty = "CSIT" /> */}
-//     {/* <Counter /> */}
-//     {/* <Post postTitle = "Learn React Basic" /> */}
-//     </>
-//   )
-// }
-
-// export default App
-
-
-// useEffect in React
-// import React, { useEffect, useState } from "react";
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   useEffect(() => {
-//     console.log("Component rendered or re-rendered");
-//   },[count]);
-
-//   return (
-//     <>
-//       <p>You Clicked {count} times</p>
-//       <button onClick={() => setCount(count+1)} >Click Me</button>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-// TodoApp using props and useState(Day4_ProfessionalClass)
-
 import React from 'react'
-import Todo from './components/Todo'
 
 const App = () => {
   return (
-    <>
-    <Todo />
-    </>
+    <div>App</div>
   )
 }
 
